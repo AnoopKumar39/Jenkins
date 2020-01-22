86ee78dd004ed5348a62ec48ed6e2d98773b43a1
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-01-22 11:01:40 +0530

    Update seed.groovy

--- seeds/seed.groovy
+++ seeds/seed.groovy
@@ -9,3 +9,15 @@ job('DSL-Tutorial-1-Test') {
         maven('-e clean test')
     }
 }
+
+job('DSL-Tutorial-2-Test') {
+    scm {
+        git('git://github.com/quidryan/aws-sdk-test.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
