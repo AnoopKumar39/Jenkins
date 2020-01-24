e8c2517d91155d6d258f3e4e1f95b8e4366dcdfa
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-01-24 12:45:39 +0530

    Update admin.groovy

--- docker-images/jenkins-master/admin.groovy
+++ docker-images/jenkins-master/admin.groovy
@@ -1,11 +1,19 @@
-/*
- * Create an admin user.
- */
 import jenkins.model.*
 import hudson.security.*
+import jenkins.security.s2m.AdminWhitelistRule
+import jenkins.model.JenkinsLocationConfiguration
+import net.sf.json.JSONObject
 
-println "--> creating admin user"
+// import jenkins.security.QueueItemAuthenticatorConfiguration
+// import org.jenkinsci.plugins.authorizeproject.GlobalQueueItemAuthenticator
+// import org.jenkinsci.plugins.authorizeproject.strategy.TriggeringUsersAuthorizationStrategy
 
+println("=== Configuring users")
+
+def instance = Jenkins.getInstance()
+String admin_email = 'raghuk.vit@gmail.com'
+JenkinsLocationConfiguration location = instance.getExtensionList('jenkins.model.JenkinsLocationConfiguration')[0]
+ 
 def adminUsername = System.getenv("ADMIN_USERNAME")
 def adminPassword = System.getenv("ADMIN_PASSWORD")
 assert adminPassword != null : "No ADMIN_USERNAME env var provided, but required"
@@ -13,9 +21,20 @@ assert adminPassword != null : "No ADMIN_PASSWORD env var provided, but required
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount(adminUsername, adminPassword)
-Jenkins.instance.setSecurityRealm(hudsonRealm)
+instance.setSecurityRealm(hudsonRealm)
+ 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-strategy.setAllowAnonymousRead(false)
-Jenkins.instance.setAuthorizationStrategy(strategy)
+instance.setAuthorizationStrategy(strategy)
+ 
+Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
+println "Updating Jenkins Email to: ${admin_email}"
+location.adminAddress = admin_email
+instance.save()
+location.save()
 
-Jenkins.instance.save()
+
+// println("=== Configure Authorize Project")
+// GlobalQueueItemAuthenticator auth = new GlobalQueueItemAuthenticator(
+//     new TriggeringUsersAuthorizationStrategy()
+// )
+// QueueItemAuthenticatorConfiguration.get().authenticators.add(auth)
