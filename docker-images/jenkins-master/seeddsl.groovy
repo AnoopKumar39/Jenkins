64bc9e40338bc232b45088f4ea6187ac795816bf
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-01-24 13:37:17 +0530

    Create seeddsl.groovy

---
+++ docker-images/jenkins-master/seeddsl.groovy
@@ -0,0 +1,21 @@
+pipelineJob('SAMPLE') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/AnoopKumar39/Jenkins.git')
+            'credentialsId'('GitUserPass')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/master')
+          }
+        }
+      }
+      'scriptPath'('jobs/pipelinejob.jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
