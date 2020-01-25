d89aaf63f56eb110a5c08285aef757d1919d0e13
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-01-25 15:20:23 +0530

    Update seed.groovy

--- seeds/seed.groovy
+++ seeds/seed.groovy
@@ -1,4 +1,4 @@
-pipelineJob('Sample') {
+pipelineJob('Sample2') {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
