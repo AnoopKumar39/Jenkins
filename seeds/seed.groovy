9e4150a5b00c9a3ed9db39b6f5af828267f50376
AnoopKumar39 <57262613+AnoopKumar39@users.noreply.github.com>
2020-02-11 14:28:14 +0530

    Update seed.groovy

--- seeds/seed.groovy
+++ seeds/seed.groovy
@@ -46,3 +46,26 @@ pipelineJob('Studentapp-Man') {
     }
   }
 }
+
+
+pipelineJob('Studentapp-Auto') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/AnoopKumar39/Jenkins.git')
+            'credentialsId'('GitUserPass')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/master')
+          }
+        }
+      }
+      'scriptPath'('jobs/release-auto.Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
